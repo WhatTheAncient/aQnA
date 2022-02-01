e6d0c1134a6784b213c8a66cc15f0d07b400092c
WhatTheAncient <whattheanc1ent@yandex.ru>
2022-02-01 15:49:30 +0300

    refactor QuestionsController and specs for it

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,13 +4,8 @@ RSpec.describe QuestionsController, type: :controller do
   let (:question) { create(:question) }
 
   describe 'GET #index' do
-    let (:questions) { create_list(:question, 3) }
-
     before { get :index }
 
-    it 'populates an array of all questions' do
-      expect(assigns(:questions)).to match_array(questions)
-    end
     it 'renders index view' do
       expect(response).to render_template :index
     end
@@ -18,10 +13,6 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'GET #show' do
     before { get :show, params: { id: question } }
-
-    it 'assigns the requested question to @question' do
-      expect(assigns(:question)).to eq question
-    end
 
     it 'renders show view' do
       expect(response).to render_template :show
@@ -31,10 +22,6 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #new' do
     before { get :new }
 
-    it 'assigns a new Question to @question' do
-      expect(assigns(:question)).to be_a_new(Question)
-    end
-
     it 'renders new view' do
       expect(response).to render_template :new
     end
@@ -42,10 +29,6 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'GET #edit'do
     before { get :edit, params: { id: question } }
-
-    it 'assigns the requested question to @question' do
-      expect(assigns(:question)).to eq question
-    end
 
     it 'renders edit view' do
       expect(response).to render_template :edit
@@ -76,11 +59,6 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'PATCH #update' do
     context 'with valid attributes' do
-      it 'assigns the requested question to @question' do
-        patch :update, params: { id: question, question: attributes_for(:question) }
-        expect(assigns(:question)).to eq question
-      end
-
       it 'changes question attributes' do
         patch :update, params: { id: question, question: { title: 'new title', body: 'new body' } }
         question.reload
