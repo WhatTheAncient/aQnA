87c018c7700ecc65a808ee6ca50ed561bd4e8673
WhatTheAncient <whattheanc1ent@yandex.ru>
2022-02-03 00:23:19 +0300

    add Devise, trying to write sign_in_spec with Capybara

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'User can sign in', %q{
+  In order to ask questions
+  As an unauthenticated user
+  I'd like to be able to sign in
+} do
+  scenario 'Registered user tries to sign in' do
+
+  end
+
+  scenario 'Registered user tries to sign in'
+end
