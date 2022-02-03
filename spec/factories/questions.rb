bdb7cf51a75419fc10c5cee1d2d726423386f687
WhatTheAncient <whattheanc1ent@yandex.ru>
2022-02-03 11:20:58 +0300

    add QuestionsController#index

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
+  sequence :title do |n|
+    "MyString#{n}"
+  end
   factory :question do
     title { "MyString" }
     body { "MyText" }
--- spec/features/question/index_spec.rb
+++ spec/features/question/index_spec.rb
@@ -5,5 +5,14 @@ feature 'User can see list of questions', %q{
   As user
   I'd like to be able to see list of all questions
 } do
+  scenario 'User go to page with all questions' do
+    questions = create_list(:question, 3)
 
+    visit questions_path
+    save_and_open_page
+    questions.each do |question|
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
+    end
+  end
 end
