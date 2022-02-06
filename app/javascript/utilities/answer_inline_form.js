917603bed3ff41191ba6f8a83c232101b008f3e7
WhatTheAncient <whattheanc1ent@yandex.ru>
2022-02-06 23:33:11 +0300

    refactor script on vanillaJS

--- app/javascript/utilities/answer_inline_form.js
+++ app/javascript/utilities/answer_inline_form.js
@@ -1,10 +1,37 @@
-$(document).on('turbolinks:load', function(){
-  console.log('file loaded')
-  $('.answers').on('click', '.edit-answer-link', function(e) {
-    e.preventDefault();
-    $(this).hide();
-    var answerId = $(this).data('answerId');
-    console.log(answerId);
-    $('form#edit-answer-' + answerId).removeClass('hidden');
-  })
-});
+document.addEventListener('turbolinks:load', function() {
+  const controls = document.querySelectorAll('.answers')
+  if (controls)
+    controls.forEach((element) => element.addEventListener('click', formInlineLinkHandler))
+
+  const errors = document.querySelector('.resource-errors')
+  if (errors) {
+    const resourceId = errors.dataset.resourceId
+    formInlineHandler(resourceId)
+  }
+})
+
+function formInlineLinkHandler(event) {
+  const editLink = this.querySelector('.edit-answer-link')
+  if (event.target === editLink) {
+    event.preventDefault()
+
+    const answerId = editLink.dataset.answerId
+    formInlineHandler(answerId)
+  }
+}
+
+function formInlineHandler(answerId) {
+  const link = document.querySelector(`.edit-answer-link[data-answer-id="${answerId}"]`)
+  const answer = document.getElementById(`${answerId}`)
+  const formInline = document.querySelector(`.inline-form[data-answer-id="${testId}"]`)
+
+  if (formInline.classList.contains('hide')) {
+    answer.querySelector('.answer-body').classList.add('hide')
+    formInline.classList.remove('hide')
+    link.textContent = 'Cancel'
+  } else {
+    answer.querySelector('.answer-body').classList.remove('hide')
+    formInline.classList.add('hide')
+    link.textContent = 'Edit'
+  }
+}
