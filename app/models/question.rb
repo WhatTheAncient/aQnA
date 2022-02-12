69307fdbb3ede7306029ce1d0535cc56bc743946
whattheanceint <whattheanc1ent@yandex.ru>
2022-02-12 11:38:58 +0300

    add files uploading to QuestionsController#create

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,7 @@ class Question < ApplicationRecord
   belongs_to :best_answer, class_name: 'Answer', foreign_key: 'best_answer_id', optional: true
   has_many :answers, dependent: :destroy
 
+  has_many_attached :files
+
   validates :title, :body, presence: true
 end
