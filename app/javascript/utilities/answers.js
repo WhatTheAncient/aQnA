3d0ba487db3d97147e7f24dfe0fb3436c35988c6
whattheanceint <whattheanc1ent@yandex.ru>
2022-02-21 09:56:19 +0300

    remove useless code

--- app/javascript/utilities/answers.js
+++ app/javascript/utilities/answers.js
@@ -2,62 +2,6 @@ document.addEventListener('turbolinks:load', function() {
     const answers = document.querySelector('.answers')
     if (answers)
         answers.addEventListener('click', formInlineLinkHandler)
-
-    $('.answers .vote-link').on('ajax:success', function(event) {
-        const vote = event.detail[0].vote
-        const rating = event.detail[0].rating
-        const answerId = vote.votable_id
-
-        $(`#answer-${answerId} .vote-link`).each(function(){ $(this).remove() })
-        $(`#answer-${answerId} .vote-rating`).html(`Rating: ${rating}`)
-
-        let cancelLink = $(`<a href=''
-                                onclick="$.ajax({
-                                    url: \`/answers/${answerId}/unvote?vote_id=${vote.id}\`,
-                                    type: 'DELETE',
-                                })"> Cancel vote </a>`)
-
-        cancelLink.addClass('btn btn-outline-danger cancel-vote-link')
-        $(`#answer-${answerId} .answer-vote`).append(cancelLink)
-    })
-
-    $('.answers .cancel-vote-link').on('ajax:success', function(event) {
-        const rating = event.detail[0].rating
-        const answerId = event.detail[0].votable_id
-
-        $(`#answer-${answerId} .cancel-vote-link`).remove()
-        $(`#answer-${answerId} .vote-rating`).html(`Rating: ${rating}`)
-
-        let upVoteLink = $(`<a href=""
-                                onclick="$.ajax({
-                                    url: \`/answers/${answerId}/vote?votable=Answer&vote_state=good\`,
-                                    type: 'POST',
-                                })"> Good </a>`)
-
-        let downVoteLink = $(`<a href=""
-                                onclick="$.ajax({
-                                    url: \`/answers/${answerId}/vote?votable=Answer&vote_state=bad\`,
-                                    type: 'POST',
-                                })"> Bad </a>`)
-
-        upVoteLink.addClass('btn btn-outline-success vote-link')
-        let br = document.createElement('br')
-        downVoteLink.addClass('btn btn-outline-danger vote-link')
-        $(`#answer-${answerId} .answer-vote`).append(upVoteLink, br, downVoteLink)
-    })
-
-    /*  Ajajson handlers
-    $('form.new-answer').on('ajax:success', function(e) {
-        const answer = e.detail[0]
-        if (answer.body) $('.answers').append(`<p> ${answer.body} </p>`)
-    })
-        .on('ajax:error', function(e) {
-            const errors = e.detail[0]
-            $.each(errors, function(index, value) {
-                $('.answer-errors').append(`<p> ${value} </p>`)
-            })
-        })
-     */
 })
 
 function formInlineLinkHandler(event) {
--- app/javascript/utilities/questions.js
+++ app/javascript/utilities/questions.js
@@ -1,49 +1,6 @@
 document.addEventListener('turbolinks:load', function() {
     const editLink = document.querySelector('.edit-question-link')
     if (editLink) editLink.addEventListener('click', formInlineHandler)
-
-    /*$('.question .vote-link').on('ajax:success', function (event) {
-        const vote = event.detail[0].vote
-        const rating = event.detail[0].rating
-        const questionId = vote.votable_id
-
-        $(`#question-${questionId} .vote-link`).each(function(){ $(this).remove() })
-        $(`#question-${questionId} .vote-rating`).html(`Rating: ${rating}`)
-
-        let cancelLink = $(`<a href="/questions"
-                                onclick="$.ajax({
-                                    url: \`/questions/${questionId}/unvote?vote_id=${vote.id}\`,
-                                    type: 'DELETE',
-                                })"> Cancel vote </a>`)
-
-        cancelLink.addClass('btn btn-danger cancel-vote-link')
-        $(`#question-${questionId} .question-vote`).append(cancelLink)
-    })
-
-    $('.question .cancel-vote-link').on('ajax:success', function (event) {
-        const rating = event.detail[0].rating
-        const questionId = event.detail[0].votable_id
-
-        $(`#question-${questionId} .cancel-vote-link`).remove()
-        $(`#question-${questionId} .vote-rating`).html(`Rating: ${rating}`)
-
-        let upVoteLink = $(`<a href="/questions"
-                                onclick="$.ajax({
-                                    url: \`/questions/${questionId}/vote?votable=Question&vote_state=good\`,
-                                    type: 'POST',
-                                })"> Good </a>`)
-
-        let downVoteLink = $(`<a href="/questions"
-                                onclick="$.ajax({
-                                    url: \`/questions/${questionId}/vote?votable=Question&vote_state=bad\`,
-                                    type: 'POST',
-                                })"> Bad </a>`)
-
-        upVoteLink.addClass('btn btn-outline-success vote-link')
-        let br = document.createElement('br')
-        downVoteLink.addClass('btn btn-outline-danger vote-link')
-        $(`#question-${questionId} .question-vote`).append(upVoteLink, br, downVoteLink)
-    })*/
 })
 
 function formInlineHandler(event) {
